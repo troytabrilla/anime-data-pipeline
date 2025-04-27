8dd360cc4305940cf94a1d975175d66b78508445
Troy Tabrilla <troy.tabrilla@gmail.com>
2025-04-27 02:24:24 -0700

    add more tests

--- tests/test_assets.py
+++ tests/test_assets.py
@@ -11,19 +11,78 @@ TEST_RAW_ANILIST_VALID = {
             "lists": [
                 {
                     "name": "test_list",
-                    "entries": [{"id": 1, "title": {"english": "test_title"}}],
-                    "status": "test_status",
+                    "entries": [
+                        {
+                            "id": 1,
+                            "media": {
+                                "id": 2,
+                                "genres": ["test_genre"],
+                                "description": "test_description",
+                                "title": {
+                                    "english": "test_title",
+                                },
+                                "status": "test_media_status",
+                                "averageScore": 50,
+                                "meanScore": 50,
+                                "popularity": 1234,
+                                "trending": 0,
+                                "favourites": 3,
+                                "episodes": 12,
+                                "tags": [
+                                    {
+                                        "category": "test_tag_category",
+                                        "description": "test_tag_description.",
+                                        "name": "test_tag_name",
+                                        "rank": 80,
+                                    },
+                                ],
+                                "stats": {},
+                                "rankings": [],
+                            },
+                            "progress": 1,
+                            "score": 7,
+                            "status": "test_entry_status",
+                            "mediaId": 2,
+                            "userId": 42,
+                        }
+                    ],
+                    "status": "test_list_status",
                 }
             ]
         },
         "User": {
-            "id": 1,
+            "id": 42,
             "name": "test_user",
         },
     }
 }
 
-TEST_RAW_ANILIST_INVALID = {"data": {}}
+TEST_RAW_ANILIST_INVALID = {
+    "data": {"MediaListCollection": {"lists": []}},
+}
+
+TEST_FACT_ANIME_VALID = [
+    {
+        "id": 1,
+        "user_id": 42,
+        "media_id": 2,
+        "average_score": 50,
+        "mean_score": 50,
+        "popularity": 1234,
+        "trending": 0,
+        "favourites": 3,
+        "progress": 1,
+        "episodes": 12,
+        "score": 7,
+        "watch_status": "test_entry_status",
+        "started_at": None,
+        "completed_at": None,
+        "stats": {},
+        "rankings": [],
+    }
+]
+
+TEST_FACT_ANIME_INVALID = []
 
 
 @mock.patch("anime_data_pipeline.defs.resources.AniListAPIResource")
@@ -52,3 +111,26 @@ def test_raw_anilist_invalid(mocked_anilist_api) -> None:
     assert validated.passed == False
     assert validated.metadata["error"].value == "raw_anilist validation failed"
     mocked_anilist_api.query.assert_called_once_with("anilist.graphql")
+
+
+@mock.patch("dagster_duckdb_pandas.DuckDBPandasIOManager")
+def test_fact_anime_valid(mocked_duckdb_io_manager) -> None:
+    actual = fact_anime(TEST_RAW_ANILIST_VALID)
+    validated = fact_anime_validate_check(actual)
+    expected = pd.DataFrame.from_dict(TEST_FACT_ANIME_VALID)
+
+    assert actual.equals(expected)
+    assert validated.passed == True
+    assert validated.metadata["count"].value == 1
+
+
+@mock.patch("dagster_duckdb_pandas.DuckDBPandasIOManager")
+def test_fact_anime_invalid(mocked_duckdb_io_manager) -> None:
+    actual = fact_anime(TEST_RAW_ANILIST_INVALID)
+    validated = fact_anime_validate_check(actual)
+    expected = pd.DataFrame.from_dict(TEST_FACT_ANIME_INVALID)
+
+    assert actual.equals(expected)
+    assert validated.passed == False
+    assert validated.metadata["count"].value == 0
+    assert validated.metadata["error"].value == "no rows processed"
